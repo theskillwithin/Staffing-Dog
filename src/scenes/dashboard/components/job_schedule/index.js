92bb5ed6dd49a3d88ddb4b63efa52baa4858f0ba
theskillwithin <austinppeterson@gmail.com>
2018-05-13 13:39:10 -0600

    dashboard days scheduled

--- src/scenes/dashboard/components/job_schedule/index.js
+++ src/scenes/dashboard/components/job_schedule/index.js
@@ -1,19 +1,34 @@
 import React from 'react'
 import Card from '@component/card'
 import Switch from '@component/switch'
+import Dropdown from '@component/Dropdown'
 
 import theme from './theme.css'
 
 
 class JobSchedule extends React.Component {
   state = {
-    switch: false,
+    form: {
+      switch: false,
+      daysScheduled: false,
+    },
   }
 
   updateSchedule = () => console.log('update schedule')
 
+  days = [
+    { label: '30 Days', value: '30' },
+    { label: '60 Days', value: '60' },
+    { label: '90 Days', value: '90' },
+    { label: '120 Days', value: '120' },
+  ]
+
   handleToggle() {
-    this.setState({ switch: !this.state.switch })
+    this.setState({ form: { ...this.state.form, switch: !this.state.switch } })
+  }
+
+  handleChange(input, value) {
+    this.setState({ form: { ...this.state.form, [input]: value } })
   }
 
   render() {
@@ -29,12 +44,20 @@ class JobSchedule extends React.Component {
         <div className={theme.inputRow}>
           <span>Same day job requests</span>
           <Switch
-            checked={state.switch}
+            checked={state.form.switch}
             onChange={() => this.handleToggle()}
             flip
           >
             No
           </Switch>
+        </div>
+        <div className={theme.inputRow}>
+          <span>Days out I can be scheduled</span>
+          <Dropdown
+            value={state.daysScheduled}
+            onChange={value => this.handleChange('daysScheduled', value)}
+            options={this.days}
+          />
         </div>
       </Card>
     )
