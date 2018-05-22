ec30211d0904fbe872716f9b8bd2ce336c685e59
theskillwithin <austinppeterson@gmail.com>
2018-05-22 14:48:59 -0600

    crfix jobschedule weeks no need for explicit return

--- src/scenes/dashboard/components/job_schedule/weeks/index.js
+++ src/scenes/dashboard/components/job_schedule/weeks/index.js
@@ -15,44 +15,42 @@ const time = [
   { label: '8:30 am', value: '8:30' },
 ]
 
-const WeekRow = ({ day, schedule, onChange }) => {
-  return (
-    <div
-      className={
-        classnames(theme.day, schedule.active && theme.active)
-      }
-    >
-      <div>
-        <Button
-          onClick={() => onChange('active', !schedule.active, day)}
-          secondary={!schedule.active}
-          round
-        >
-          {day.toUpperCase()}
-        </Button>
+const WeekRow = ({ day, schedule, onChange }) => (
+  <div
+    className={
+      classnames(theme.day, schedule.active && theme.active)
+    }
+  >
+    <div>
+      <Button
+        onClick={() => onChange('active', !schedule.active, day)}
+        secondary={!schedule.active}
+        round
+      >
+        {day.toUpperCase()}
+      </Button>
+    </div>
+    <div className={theme.dropdowns}>
+      <div className={theme.dropdown}>
+        <Dropdown
+          value={schedule.from}
+          onChange={value => onChange('from', value, day)}
+          options={time}
+          box={false}
+        />
       </div>
-      <div className={theme.dropdowns}>
-        <div className={theme.dropdown}>
-          <Dropdown
-            value={schedule.from}
-            onChange={value => onChange('from', value, day)}
-            options={time}
-            box={false}
-          />
-        </div>
-        <Icon secondary use="arrow_forward" />
-        <div className={theme.dropdown}>
-          <Dropdown
-            value={schedule.to}
-            onChange={value => onChange('to', value, day)}
-            options={time}
-            box={false}
-          />
-        </div>
+      <Icon secondary use="arrow_forward" />
+      <div className={theme.dropdown}>
+        <Dropdown
+          value={schedule.to}
+          onChange={value => onChange('to', value, day)}
+          options={time}
+          box={false}
+        />
       </div>
     </div>
-  )
-}
+  </div>
+)
 
 WeekRow.defaultProps = ({
   day: false,
