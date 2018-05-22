ddbcd14c88bd1773c3c0c0458cad8bb31f91ed0b
theskillwithin <austinppeterson@gmail.com>
2018-05-22 15:37:21 -0600

    checklist use isRequired instead of defaultProp

--- src/components/checklist/index.js
+++ src/components/checklist/index.js
@@ -1,35 +1,25 @@
 import React from 'react'
-import { array, bool, oneOfType } from 'prop-types'
+import { array } from 'prop-types'
 import classnames from 'classnames'
 
 import theme from './theme.css'
 
 
-const Checklist = ({ list }) => {
-  if (!list) return null
-  return (
-    <ul className={theme.ul}>
-      {list.map((li, i) => (
-        <li
-          key={`li-${li.name}${i + 1}`}
-          className={classnames(theme.li, li.checked && theme.checked)}
-        >
-          {li.name}
-        </li>
-      ))}
-    </ul>
-  )
-}
-
-Checklist.defaultProps = {
-  list: false,
-}
+const Checklist = ({ list }) => (
+  <ul className={theme.ul}>
+    {list.map((li, i) => (
+      <li
+        key={`li-${li.name}${i + 1}`}
+        className={classnames(theme.li, li.checked && theme.checked)}
+      >
+        {li.name}
+      </li>
+    ))}
+  </ul>
+)
 
 Checklist.propTypes = {
-  list: oneOfType([
-    array,
-    bool,
-  ]),
+  list: array.isRequired,
 }
 
 export default Checklist
