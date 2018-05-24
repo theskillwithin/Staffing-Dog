8e4d3d461e90ef3e70a0410f1e6dbcaacedc7e5e
theskillwithin <austinppeterson@gmail.com>
2018-05-23 18:04:55 -0600

    settings profile start making tabs

--- src/scenes/settings/index.js
+++ src/scenes/settings/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import classnames from 'classnames'
+import { TabBar, Tab } from 'rmwc/Tabs'
 import Card from '@component/card'
 
 import theme from '../app/theme.css'
@@ -7,12 +8,36 @@ import theme from '../app/theme.css'
 import Header from './components/header'
 
 
-const Settings = () => (
-  <div className={classnames(theme.pageContent)}>
-    <Card type="large">
-      <Header />
-    </Card>
-  </div>
-)
+class Settings extends React.Component {
+  state = {
+    activeTabIndex: 0,
+  }
+
+  render() {
+    return (
+      <div className={classnames(theme.pageContent)}>
+        <Card type="large">
+          <Header />
+          <TabBar
+            activeTabIndex={this.state.activeTabIndex}
+            onChange={evt => this.setState({ activeTabIndex: evt.target.value })}
+          >
+            <Tab>About Me</Tab>
+            <Tab>My Resume</Tab>
+            <Tab>References</Tab>
+            <Tab>Notifications</Tab>
+            <Tab>Security</Tab>
+          </TabBar>
+
+          {this.state.activeTabIndex === 0 && <h1>About Me</h1>}
+          {this.state.activeTabIndex === 1 && <h1>My Resume</h1>}
+          {this.state.activeTabIndex === 2 && <h1>References</h1>}
+          {this.state.activeTabIndex === 3 && <h1>Notifications</h1>}
+          {this.state.activeTabIndex === 4 && <h1>Security</h1>}
+        </Card>
+      </div>
+    )
+  }
+}
 
 export default Settings
