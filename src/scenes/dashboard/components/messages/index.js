d727659c94c939bc10576d972685e21050bbbfff
theskillwithin <austinppeterson@gmail.com>
2018-06-02 16:18:20 -0600

    messages mock data message list styles

--- src/scenes/dashboard/components/messages/index.js
+++ src/scenes/dashboard/components/messages/index.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import map from 'lodash/map'
+import classnames from 'classnames'
+import ProfilePhotoSVG from '@component/svg/ProfilePhoto'
 
 import theme from './theme.css'
 
@@ -7,10 +10,93 @@ class Messages extends React.Component {
     active: 0,
   }
 
+  messages = [
+    {
+      id: 0,
+      from: 'Wes Bos',
+      location: 'React Dental',
+      date: '5/16/18',
+      short:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, dolor provident.',
+      read: false,
+      threadCount: 4,
+    },
+    {
+      id: 1,
+      from: 'Wes Bos',
+      location: 'React Dental',
+      date: '5/16/18',
+      short:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, dolor provident.',
+      read: false,
+      threadCount: null,
+    },
+    {
+      id: 2,
+      from: 'Wes Bos',
+      location: 'React Dental',
+      date: '5/16/18',
+      short:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, dolor provident.',
+      read: true,
+      threadCount: null,
+    },
+    {
+      id: 3,
+      from: 'Wes Bos',
+      date: '5/16/18',
+      short:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, dolor provident.',
+      read: true,
+      threadCount: null,
+    },
+    {
+      id: 4,
+      from: 'Wes Bos',
+      location: 'React Dental',
+      date: '5/16/18',
+      short:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, dolor provident.',
+      read: true,
+      threadCount: null,
+      avatar: 'https://fillmurray.com/48/48',
+    },
+  ]
+
   render() {
     return (
       <div className={theme.messages}>
-        <h1>Hello World</h1>
+        {map(this.messages, message => (
+          <div
+            key={message.id}
+            className={classnames(theme.message, !message.read && theme.unread)}
+          >
+            <div className={theme.avatar}>
+              {message.avatar ? (
+                <img src={message.avatar} alt="avatar" />
+              ) : (
+                <ProfilePhotoSVG />
+              )}
+            </div>
+            <div className={theme.middle}>
+              <div className={theme.title}>
+                <h6>{message.from}</h6>
+                {message.location && <span>{message.location}</span>}
+              </div>
+              <div className={theme.short}>
+                <p>{message.short && message.short}</p>
+              </div>
+            </div>
+            <div className={theme.right}>
+              <div className={theme.date}>{message.date}</div>
+              {message.threadCount &&
+                message.threadCount > 1 && (
+                  <div className={theme.threadCount}>{message.threadCount}</div>
+                )}
+              <div className={theme.reply}>R</div>
+            </div>
+          </div>
+        ))}
       </div>
     )
   }
