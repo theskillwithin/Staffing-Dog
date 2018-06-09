f51857d8f99e1e5f5598f81643bd62fa667a0ea3
theskillwithin <austinppeterson@gmail.com>
2018-06-09 13:02:03 -0600

    calendar style tweaks

--- src/scenes/dashboard/components/job_schedule/calendar/index.js
+++ src/scenes/dashboard/components/job_schedule/calendar/index.js
@@ -14,7 +14,7 @@ class Calendar extends React.Component {
   }
 
   activeDates = [
-    { startDate: '2018-06-02', endDate: '2018-06-04', primary: true },
+    { startDate: '2018-06-02', endDate: '2018-06-05', primary: true },
     { startDate: '2018-06-12', endDate: '2018-06-15', primary: false },
     { startDate: '2018-06-20', endDate: '2018-06-20', primary: true },
     { startDate: '2018-07-02', endDate: '2018-07-04', primary: true },
