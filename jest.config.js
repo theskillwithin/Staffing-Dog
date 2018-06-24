a5a4d944e8d22cb447873e14663856a4d0b7a1b5
Will Ayers <willayers.net@gmail.com>
2018-06-24 10:57:20 -0600

    Updated jest to ignore config folder for coverage files. Added script commands for tests and coverage.

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   rootDir: './',
   coverageDirectory: '<rootDir>/public/_coverage',
   moduleDirectories: ['node_modules', 'src'],
+  coveragePathIgnorePatterns: ['<rootDir>/config'],
   testRegex: '(/__tests__/.*|\\.(spec))\\.(jsx?)$',
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
@@ -9,6 +10,7 @@ module.exports = {
     '\\theme.(css)$': 'identity-obj-proxy',
     '\\.(css)$': '<rootDir>/__mocks__/styleMock.js',
     '^@sd': '<rootDir>/src',
+    '^@sdog': '<rootDir>/src',
     '^@scene': '<rootDir>/src/scenes',
     '^@store': '<rootDir>/src/store',
     '^@api': '<rootDir>/src/api',
