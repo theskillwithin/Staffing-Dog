c5b93e1edf2d97072124d47dd6b75d76374ea16d
theskillwithin <austinppeterson@gmail.com>
2018-07-05 14:27:04 -0600

    start login page

--- src/scenes/login/index.js
+++ src/scenes/login/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
+import classnames from 'classnames'
 import { setTitle, setHtmlClass, removeHtmlClass } from '@sdog/utils/document'
+import Contact from '@scene/app/contact'
+
+import appTheme from '../app/theme.css'
+
+import theme from './theme.css'
 
 class Login extends React.Component {
   componentDidMount() {
@@ -12,8 +18,13 @@ class Login extends React.Component {
   }
 
   render = () => (
-    <div>
-      <h2>Login</h2>
+    <div className={classnames(appTheme.pageContent, appTheme.columns)}>
+      <header className={theme.header}>
+        <Contact />
+      </header>
+      <div className={theme.signin}>
+        <h2>Sign In</h2>
+      </div>
     </div>
   )
 }
