44048f295d0d56b71b77637bd0d3c0c6e153100d
theskillwithin <austinppeterson@gmail.com>
2018-08-27 17:44:33 -0600

    login page toggle hamburger menu on mobile

--- src/scenes/app/contact/index.js
+++ src/scenes/app/contact/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import classnames from 'classnames'
 import ContactSVG from '@component/svg/Contact'
 import HeadsetSVG from '@component/svg/Headset'
 import EmailUsSVG from '@component/svg/EmailUs'
@@ -8,35 +9,56 @@ import { Link } from 'react-router-dom'
 
 import theme from './theme.css'
 
-const Contact = () => (
-  <div className={theme.contact}>
-    <div className={theme.contactInner}>
-      <div className={theme.contactDesktop}>
-        <Link to="/contact">
-          <ContactSVG className={theme.contactSVG} /> Contact Us
-        </Link>
+class Contact extends React.Component {
+  state = {
+    mobileActive: false,
+  }
+
+  handleMobileToggle = () => {
+    this.setState(state => ({ mobileActive: !state.mobileActive }))
+  }
+
+  render() {
+    return (
+      <div className={theme.contact}>
+        <div className={theme.contactInner}>
+          <div className={theme.contactDesktop}>
+            <Link to="/contact">
+              <ContactSVG className={theme.contactSVG} /> Contact Us
+            </Link>
+          </div>
+          <button
+            onClick={this.handleMobileToggle}
+            className={theme.contactMobile}
+            type="button"
+          >
+            {this.state.mobileActive ? <Icon use="close" /> : <Icon use="menu" />}
+          </button>
+        </div>
+        <div
+          className={classnames(
+            theme.contactActive,
+            this.state.mobileActive && theme.mobileActive,
+          )}
+        >
+          <div className={theme.contactActiveInner}>
+            <a href="/test">
+              <HeadsetSVG />
+              (385) 707-0156
+            </a>
+            <a href="/test">
+              <EmailUsSVG />
+              Email Us
+            </a>
+            <a href="/test">
+              <LiveChatSVG />
+              Live Chat
+            </a>
+          </div>
+        </div>
       </div>
-      <div className={theme.contactMobile}>
-        <Icon use="menu" />
-      </div>
-    </div>
-    <div className={theme.contactActive}>
-      <div className={theme.contactActiveInner}>
-        <a href="/test">
-          <HeadsetSVG />
-          (385) 707-0156
-        </a>
-        <a href="/test">
-          <EmailUsSVG />
-          Email Us
-        </a>
-        <a href="/test">
-          <LiveChatSVG />
-          Live Chat
-        </a>
-      </div>
-    </div>
-  </div>
-)
+    )
+  }
+}
 
 export default Contact
