c70c3ee17584824b0d21fd8a0334b1aeb3c23d56
theskillwithin <austinppeterson@gmail.com>
2018-09-23 15:35:32 -0600

    main menu contact mobile hamburger toggle

--- src/scenes/app/menu/index.js
+++ src/scenes/app/menu/index.js
@@ -2,12 +2,24 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { object } from 'prop-types'
 import classnames from 'classnames'
+import Icon from '@component/icon'
 
 import theme from './theme.css'
 
 class MainMenu extends React.Component {
   state = {
     mobileActive: false,
+  }
+
+  handleMobileToggle = () => {
+    this.setState(state => ({ mobileActive: !state.mobileActive }))
+  }
+
+  handleClickOutside = () => {
+    if (this.state.mobileActive) {
+      return this.handleMobileToggle()
+    }
+    return false
   }
 
   isActive(page) {
@@ -17,41 +29,52 @@ class MainMenu extends React.Component {
 
   render() {
     return (
-      <ul className={theme.nav}>
-        <li className={theme.navItem}>
-          <Link
-            className={classnames(
-              theme.navItemLink,
-              this.isActive(/^\/$/) && theme.active,
-            )}
-            to="/"
-          >
-            Dashboard
-          </Link>
-        </li>
-        <li className={theme.navItem}>
-          <Link
-            className={classnames(
-              theme.navItemLink,
-              this.isActive(/search/) && theme.active,
-            )}
-            to="/search"
-          >
-            Job&nbsp;Search
-          </Link>
-        </li>
-        <li className={theme.navItem}>
-          <Link
-            className={classnames(
-              theme.navItemLink,
-              this.isActive(/settings/) && theme.active,
-            )}
-            to="/settings"
-          >
-            My&nbsp;Profile
-          </Link>
-        </li>
-      </ul>
+      <div className={theme.navContainer}>
+        <button
+          onClick={this.handleMobileToggle}
+          className={theme.contactMobile}
+          type="button"
+        >
+          {this.state.mobileActive ? <Icon use="close" /> : <Icon use="menu" />}
+        </button>
+        <ul
+          className={classnames(theme.nav, this.state.mobileActive && theme.mobileActive)}
+        >
+          <li className={theme.navItem}>
+            <Link
+              className={classnames(
+                theme.navItemLink,
+                this.isActive(/^\/$/) && theme.active,
+              )}
+              to="/"
+            >
+              Dashboard
+            </Link>
+          </li>
+          <li className={theme.navItem}>
+            <Link
+              className={classnames(
+                theme.navItemLink,
+                this.isActive(/search/) && theme.active,
+              )}
+              to="/search"
+            >
+              Job&nbsp;Search
+            </Link>
+          </li>
+          <li className={theme.navItem}>
+            <Link
+              className={classnames(
+                theme.navItemLink,
+                this.isActive(/settings/) && theme.active,
+              )}
+              to="/settings"
+            >
+              My&nbsp;Profile
+            </Link>
+          </li>
+        </ul>
+      </div>
     )
   }
 }
