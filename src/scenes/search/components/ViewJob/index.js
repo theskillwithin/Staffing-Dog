8a09ca0e7e4bf42d9dac99431a7201c4d0c5754c
theskillwithin <austinppeterson@gmail.com>
2018-10-05 14:14:00 -0600

    job page title section

--- src/scenes/search/components/ViewJob/index.js
+++ src/scenes/search/components/ViewJob/index.js
@@ -7,7 +7,22 @@ import theme from './theme.css'
 
 const ViewJob = () => (
   <div className={classnames(appTheme.pageContent, theme.pageContent)}>
-    <p>Single Job View</p>
+    <section className={theme.title}>
+      <img src="http://fillmurray.com/133/92" alt="Job Logo" />
+      <div>
+        <h2>Temporary Hygienist Needed for Maternity Leave</h2>
+        <h4>APEX Dental</h4>
+      </div>
+    </section>
+    <section>
+      <p>
+        We are in need of a RDH for the months of December, January and February for
+        maternity leave. Possibly turning into a part-time permanet position. Dentrix
+        knowlege is preferred. Professional position responsible to provide dental
+        prophylaxis, application of fluoride solutions, varnishes, and sealants. Assists
+        the dentist as required (i.e. x-rays, impressions, etc)
+      </p>
+    </section>
   </div>
 )
 
