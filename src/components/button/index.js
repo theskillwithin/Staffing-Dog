e79bfea12e51d283a5e501156ffeb1001f8dfbc8
theskillwithin <austinppeterson@gmail.com>
2018-10-31 13:45:38 -0600

    update deps and start refactoring button

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,39 +1,63 @@
 import React from 'react'
-import { bool, string, oneOfType } from 'prop-types'
-import { Button as MButton } from 'rmwc/Button'
+import { bool, string, oneOfType, func, node, oneOf } from 'prop-types'
 import classnames from 'classnames'
 
 import './styles.css'
 import theme from './theme.css'
 
-const Button = ({ primary, secondary, round, short, className, ...props }) => (
-  <MButton
+const Button = ({
+  primary,
+  secondary,
+  round,
+  short,
+  className,
+  onClick,
+  disabled,
+  children,
+  type,
+  ...props
+}) => (
+  /* eslint-disable  react/button-has-type */
+  <button
     className={classnames(
       theme.button,
       round && theme.round,
       short && theme.short,
       className && className,
     )}
-    unelevated={primary || secondary}
-    theme={secondary ? 'secondary-bg' : ''}
+    onClick={onClick}
+    type={type}
     {...props}
-  />
+  >
+    {children}
+  </button>
 )
 
+/* eslint-enable */
+
+// theme={secondary ? 'secondary-bg' : ''}
+// unelevated={primary || secondary}
+
 Button.defaultProps = {
+  type: 'submit',
   primary: true,
   secondary: false,
   round: false,
   short: false,
   className: false,
+  disabled: false,
 }
 
 Button.propTypes = {
+  onClick: func.isRequired,
   primary: bool,
   secondary: bool,
   round: bool,
   short: bool,
   className: oneOfType([string, bool]),
+  children: oneOfType([string, node]),
+  disabled: bool,
+  type: oneOf(['button', 'submit', 'reset']),
 }
 
 export default Button
