1491eb11272fe30b7d5d2bb88a12bff6e659bc63
theskillwithin <austinppeterson@gmail.com>
2018-11-03 13:55:28 -0600

    settings rm My Resume tab

--- src/scenes/settings/index.js
+++ src/scenes/settings/index.js
@@ -33,14 +33,12 @@ class Settings extends React.Component {
             settingsTabs
           >
             <div>About Me</div>
-            <div>My Resume</div>
             <div>References</div>
             <div>Notifications</div>
             <div>Security</div>
           </Tabs>
 
           {this.state.activeTabIndex === 0 && <AboutMe />}
-          {this.state.activeTabIndex === 1 && <h1>My Resume</h1>}
           {this.state.activeTabIndex === 2 && <h1>References</h1>}
           {this.state.activeTabIndex === 3 && <h1>Notifications</h1>}
           {this.state.activeTabIndex === 4 && <h1>Security</h1>}
