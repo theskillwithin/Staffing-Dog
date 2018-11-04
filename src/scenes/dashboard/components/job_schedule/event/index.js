6090a80be4172b7ef19418aa8bfd8b05e20f6b3f
Will Ayers <willayers.net@gmail.com>
2018-11-04 09:18:13 -0700

    Updated schedule sidebar to be able to toggle updating your schedule. Auto open first event to map.

--- src/scenes/dashboard/components/job_schedule/event/index.js
+++ src/scenes/dashboard/components/job_schedule/event/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { object } from 'prop-types'
+import { object, bool } from 'prop-types'
 import classnames from 'classnames'
 import NearMeIcon from '@component/svg/NearMe'
 import Map from '@component/map'
@@ -7,30 +7,44 @@ import Map from '@component/map'
 import theme from './theme.css'
 
 class JobScheduleEvent extends React.Component {
-  state = {
+  static propTypes = {
+    event: object.isRequired,
+    open: bool,
+  }
+
+  static defaultProps = {
     open: false,
   }
 
-  handleClick = () => {
-    this.setState(state => ({ open: !state.open }))
-  }
+  state = { open: this.props.open || false }
+
+  handleClick = () => this.setState(state => ({ open: !state.open }))
 
   render() {
-    const { event } = this.props
+    const {
+      handleClick,
+      state,
+      props: { event },
+    } = this
+
     if (!event) return null
+
     return (
       <div>
-        <button type="button" className={theme.event} onClick={this.handleClick}>
+        <button type="button" className={theme.event} onClick={handleClick}>
           <h2 className={event.type && theme[event.type]}>{event.date}</h2>
+
           <div className={theme.eventDetails}>
             <h5>
               {event.location} @ {event.time}
             </h5>
             <h6>{event.address}</h6>
           </div>
+
           <NearMeIcon />
         </button>
-        <div className={classnames(theme.map, this.state.open && theme.open)}>
+
+        <div className={classnames(theme.map, state.open && theme.open)}>
           <Map
             isMarkerShown
             position={{ lat: 40.764411, lng: -111.891559 }}
@@ -40,10 +54,6 @@ class JobScheduleEvent extends React.Component {
       </div>
     )
   }
-}
-
-JobScheduleEvent.propTypes = {
-  event: object.isRequired,
 }
 
 export default JobScheduleEvent
