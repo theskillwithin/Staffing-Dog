2bd929097e4f8f930c7600ff45efe82c6a180b44
theskillwithin <austinppeterson@gmail.com>
2018-11-12 18:41:27 -0700

    job schedule icon dynamic date

--- src/components/svg/Calendar/index.js
+++ src/components/svg/Calendar/index.js
@@ -1,24 +1,50 @@
 import React from 'react'
 import { string } from 'prop-types'
 import classnames from 'classnames'
-
-import SVGCheck from '../files/calendar.svg'
+import moment from 'moment'
 
 import theme from './theme.css'
 
-const Calendar = ({ className }) => (
-  <span
+const Calendar = ({ className, date }) => (
+  <svg
     className={classnames(className, theme.svg)}
-    dangerouslySetInnerHTML={{ __html: SVGCheck }}
-  />
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 18 20"
+  >
+    <g id="Group_421" data-name="Group 421" transform="translate(-829 -135)">
+      <text
+        id="_21"
+        data-name="21"
+        className={classnames(theme.fill, theme.text)}
+        transform="translate(829 152)"
+        x="50%"
+        y="-3px"
+        alignmentBaseline="middle"
+        textAnchor="middle"
+      >
+        {date}
+      </text>
+      <rect
+        id="Rectangle_76"
+        data-name="Rectangle 76"
+        className={theme.fill}
+        width="100%"
+        height="2"
+        rx="1"
+        transform="translate(829 135)"
+      />
+    </g>
+  </svg>
 )
 
 Calendar.defaultProps = {
   className: '',
+  date: moment().format('DD') || '21',
 }
 
 Calendar.propTypes = {
   className: string,
+  date: string,
 }
 
 export default Calendar
