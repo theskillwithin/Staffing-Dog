15272833e22755ac51a0ccc797e30ca3833f07d7
theskillwithin <austinppeterson@gmail.com>
2018-12-11 17:19:36 -0700

    cypress first test step 1 on onboarding

--- cypress/e2e/onboarding.js
+++ cypress/e2e/onboarding.js
@@ -1,7 +1,9 @@
-describe('yay', () => {
-  it('can yay', () => {
+describe('onboarding navigation', () => {
+  it('can navigate to step 1', () => {
     cy.visit('/#/onboarding')
       .getByText(/Let's Get Started/)
       .click()
+      .url()
+      .should('include', 'onboarding/professional/step/1')
   })
 })
