f1e40b57e6c08a8687af7b7d4ea0e7860ec64e73
theskillwithin <austinppeterson@gmail.com>
2018-12-13 19:13:00 -0700

    onboarding setup complete mobile layout fixes

--- src/scenes/onboarding/components/layout/Layout.js
+++ src/scenes/onboarding/components/layout/Layout.js
@@ -51,6 +51,8 @@ class Onboarding extends Component {
     const hasStep = RegExp('/step/([0-9]+)')
     const showSidebar = hasStep.test(location.pathname)
 
+    console.log(/complete/.test(location.pathname))
+
     return (
       <div className={theme.app}>
         <div className={theme.appInner}>
@@ -80,7 +82,12 @@ class Onboarding extends Component {
             </div>
           </div>
 
-          <div className={theme.appContent}>
+          <div
+            className={classnames(
+              theme.appContent,
+              /complete/.test(location.pathname) && theme.appContentComplete,
+            )}
+          >
             <div className={classnames(theme.box, showSidebar && theme.showSidebar)}>
               <div className={theme.stepContent}>
                 <div className={theme.stepLinksHolder}>
