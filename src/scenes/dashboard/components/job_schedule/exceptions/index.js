abf96a6b356a348811a272dc7c4a513e2d636b1d
theskillwithin <austinppeterson@gmail.com>
2018-12-21 17:08:50 -0700

    add available switch

--- src/scenes/dashboard/components/job_schedule/exceptions/index.js
+++ src/scenes/dashboard/components/job_schedule/exceptions/index.js
@@ -1,14 +1,32 @@
 import React from 'react'
+import Switch from '@component/switch'
 
 import theme from './theme.css'
 
 class Exceptions extends React.Component {
   state = {
-    form: {},
+    form: {
+      switch: false,
+    },
+  }
+
+  handleToggle = value => {
+    this.setState(state => ({ form: { ...state.form, switch: value } }))
   }
 
   render() {
-    return <div className={theme.root}>exceptions....</div>
+    return (
+      <div className={theme.root}>
+        <h4>Add a new availability exception</h4>
+
+        <div className={theme.inputRow}>
+          <span>Available</span>
+          <Switch checked={this.state.form.switch} onChange={this.handleToggle}>
+            {this.state.form.switch ? 'Yes' : 'No'}
+          </Switch>
+        </div>
+      </div>
+    )
   }
 }
 
