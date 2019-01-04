bfad711caa07504eeed89b3fb1b8575f2a38eafd
Will Ayers <willayers.net@gmail.com>
2019-01-03 19:39:42 -0700

    Added build command and prod build commands to use different urls for the api.

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,6 +1,7 @@
 export const ENV = process.env.NODE_ENV
 export const IS_DEV = ENV === 'development'
 export const IS_PROD = !ENV
+export const IS_STAGE = process.env.BUILD_STAGE || false
 export const USE_MOCK = Boolean(process.env.MOCK_DATA)
 // export const SIM = IS_DEV || USE_MOCK
 export const SIM = true
