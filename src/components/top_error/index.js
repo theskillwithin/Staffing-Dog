b1b1c3d30d3088ef886bc4ead03e889394c47798
theskillwithin <austinppeterson@gmail.com>
2019-02-09 23:40:00 -0700

    handle mutliple errors with a delay

--- src/components/top_error/index.js
+++ src/components/top_error/index.js
@@ -1,20 +1,41 @@
-import React from 'react'
-import { string, array, number, oneOfType } from 'prop-types'
+import React, { useState, useEffect } from 'react'
+import { string, array, number, bool, oneOfType } from 'prop-types'
 
 import theme from './theme.css'
 
-const distanceToTop = 30
-const heightOfError = 49
-const marginBetween = 10
-
-export const TopError = ({ children, multiple }) => {
+export const TopError = ({ children, multiple, closeButton, autoClose }) => {
   if (!children) return null
-  const top = multiple
-    ? distanceToTop + (multiple * heightOfError + multiple * marginBetween)
-    : distanceToTop
+  const [delayed, setDelayed] = useState(multiple)
+  const [isClose, setClose] = useState(false)
+
+  useEffect(() => {
+    if (autoClose) {
+      setTimeout(() => {
+        setClose(true)
+      }, 10000)
+    }
+    if (!multiple) return
+
+    setTimeout(() => {
+      setDelayed(false)
+    }, multiple * 300)
+  })
+
+  if (isClose) return null
+  if (delayed) return null
+
   return (
-    <div className={theme.topError} style={{ top }}>
+    <div className={theme.topError}>
       <p>{children}</p>
+      {closeButton ? (
+        <button
+          className={theme.closeButton}
+          onClick={() => setClose(true)}
+          type="button"
+        >
+          &times;
+        </button>
+      ) : null}
     </div>
   )
 }
@@ -22,37 +43,56 @@ export const TopError = ({ children, multiple }) => {
 TopError.defaultProps = {
   children: null,
   multiple: null,
+  autoClose: false,
+  closeButton: false,
 }
 
 TopError.propTypes = {
   children: string,
   multiple: number,
+  autoClose: bool,
+  closeButton: bool,
 }
 
-const MultipleTopError = ({ children }) => {
+const MultipleTopError = ({ children, autoClose, closeButton }) => {
   if (!children) return null
 
   if (typeof children === 'string' || (children.length && children.length === 1)) {
-    return <TopError>{children}</TopError>
+    return (
+      <div className={theme.topErrorContainer}>
+        <TopError autoClose={autoClose} closeButton={closeButton}>
+          {children}
+        </TopError>
+      </div>
+    )
   }
 
   return (
-    <>
-      {test.map((singleChild, index) => (
-        <TopError key={`toperror-${singleChild.charAt(0)}-${index + 1}`} multiple={index}>
+    <div className={theme.topErrorContainer}>
+      {children.map((singleChild, index) => (
+        <TopError
+          key={`toperror-${singleChild.charAt(0)}-${index + 1}`}
+          multiple={index}
+          autoClose={autoClose}
+          closeButton
+        >
           {singleChild}
         </TopError>
       ))}
-    </>
+    </div>
   )
 }
 
 MultipleTopError.defaultProps = {
   children: null,
+  autoClose: false,
+  closeButton: false,
 }
 
 MultipleTopError.propTypes = {
   children: oneOfType([string, array]),
+  autoClose: bool,
+  closeButton: bool,
 }
 
 export default MultipleTopError
