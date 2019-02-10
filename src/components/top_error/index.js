7b9f11edea35a6eec78d6e128dcfbfb40e34ab2b
theskillwithin <austinppeterson@gmail.com>
2019-02-09 22:36:25 -0700

    handle multiple errors for top error

--- src/components/top_error/index.js
+++ src/components/top_error/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import { string } from 'prop-types'
+import { string, array, number, oneOfType } from 'prop-types'
 
 import theme from './theme.css'
 
-const TopError = ({ children }) => {
+const distanceToTop = 30
+const heightOfError = 49
+const marginBetween = 10
+
+export const TopError = ({ children, multiple }) => {
   if (!children) return null
+  const top = multiple
+    ? distanceToTop + (multiple * heightOfError + multiple * marginBetween)
+    : distanceToTop
   return (
-    <div className={theme.topError}>
+    <div className={theme.topError} style={{ top }}>
       <p>{children}</p>
     </div>
   )
@@ -14,10 +21,38 @@ const TopError = ({ children }) => {
 
 TopError.defaultProps = {
   children: null,
+  multiple: null,
 }
 
 TopError.propTypes = {
   children: string,
+  multiple: number,
 }
 
-export default TopError
+const MultipleTopError = ({ children }) => {
+  if (!children) return null
+
+  if (typeof children === 'string' || (children.length && children.length === 1)) {
+    return <TopError>{children}</TopError>
+  }
+
+  return (
+    <>
+      {test.map((singleChild, index) => (
+        <TopError key={`toperror-${singleChild.charAt(0)}-${index + 1}`} multiple={index}>
+          {singleChild}
+        </TopError>
+      ))}
+    </>
+  )
+}
+
+MultipleTopError.defaultProps = {
+  children: null,
+}
+
+MultipleTopError.propTypes = {
+  children: oneOfType([string, array]),
+}
+
+export default MultipleTopError
