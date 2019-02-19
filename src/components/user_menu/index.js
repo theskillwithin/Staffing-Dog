423ba64ca5f84ea4b03278c7ed2eabe43c60e2c3
theskillwithin <austinppeterson@gmail.com>
2019-02-19 16:36:57 -0700

    make user menu useState

--- src/components/user_menu/index.js
+++ src/components/user_menu/index.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import enhanceWithClickOutside from 'react-click-outside'
+import React, { useState, useRef } from 'react'
+import useOutsideClick from '@sdog/utils/useOutsideClick'
 import clsx from 'clsx'
 import Arrow from '@sdog/components/svg/Arrow'
 import CloseIcon from '@sdog/components/svg/Close'
@@ -8,67 +8,50 @@ import { Link } from 'react-router-dom'
 
 import theme from './theme.css'
 
-class UserMenu extends React.Component {
-  state = {
-    mobileActive: false,
-  }
-
-  componentDidMount() {
-    if ('ontouchstart' in document.documentElement) {
-      document.body.style.cursor = 'pointer'
-    }
-  }
-
-  handleMobileToggle = () => {
-    this.setState(state => ({ mobileActive: !state.mobileActive }))
-  }
-
-  handleClickOutside = () => {
-    if (this.state.mobileActive) {
-      return this.handleMobileToggle()
+const UserMenu = () => {
+  const [mobileActive, setMobileActive] = useState(false)
+  const pRef = useRef()
+  const handleMobileToggle = () => setMobileActive(!mobileActive)
+  const handleClickOutside = () => {
+    if (mobileActive) {
+      return handleMobileToggle()
     }
     return false
   }
+  useOutsideClick(pRef, handleClickOutside)
 
-  render() {
-    return (
-      <div className={theme.userMenu}>
-        <div className={theme.userMenuInner}>
-          <div className={theme.userMenuDesktop}>
+  return (
+    <div className={theme.userMenu}>
+      <div className={theme.userMenuInner} ref={pRef}>
+        <div className={theme.userMenuDesktop}>
+          <Link to="/contact">
+            <Arrow /> Contact Us
+          </Link>
+        </div>
+        <button
+          onClick={handleMobileToggle}
+          className={theme.userMenuMobile}
+          type="button"
+        >
+          {mobileActive ? <CloseIcon /> : <Hambuger />}
+        </button>
+      </div>
+      <div className={clsx(theme.userMenuActive, mobileActive && theme.mobileActive)}>
+        <div className={theme.userMenuActiveInner}>
+          <div className={theme.mobileOption}>
             <Link to="/contact">
-              <Arrow /> Contact Us
+              <Arrow /> Debbie B.
             </Link>
           </div>
-          <button
-            onClick={this.handleMobileToggle}
-            className={theme.userMenuMobile}
-            type="button"
-          >
-            {this.state.mobileActive ? <CloseIcon /> : <Hambuger />}
-          </button>
-        </div>
-        <div
-          className={clsx(
-            theme.userMenuActive,
-            this.state.mobileActive && theme.mobileActive,
-          )}
-        >
-          <div className={theme.userMenuActiveInner}>
-            <div className={theme.mobileOption}>
-              <Link to="/contact">
-                <Arrow /> Debbie B.
-              </Link>
-            </div>
-            <a href="/test">Profile</a>
-            <a href="/test">Billing</a>
-            <a href="/test">Users</a>
-            <a href="/test">Account</a>
-            <a href="/test">Logout</a>
-          </div>
+          <a href="/test">Profile</a>
+          <a href="/test">Billing</a>
+          <a href="/test">Users</a>
+          <a href="/test">Account</a>
+          <a href="/test">Logout</a>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
-export default enhanceWithClickOutside(UserMenu)
+export default UserMenu
