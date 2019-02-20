1f4244a4dd55241a85bbffd78fe9c1c4dbf7e56d
theskillwithin <austinppeterson@gmail.com>
2019-02-19 17:14:58 -0700

    make mobile main menu at bottom of screen

--- src/scenes/app/menu/index.js
+++ src/scenes/app/menu/index.js
@@ -1,41 +1,23 @@
-import React, { useState, useRef } from 'react'
-import useOutsideClick from '@sdog/utils/useOutsideClick'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { object } from 'prop-types'
 import clsx from 'clsx'
-import CloseIcon from '@sdog/components/svg/Close'
-import Hambuger from '@sdog/components/svg/Hamburger'
 
 import theme from './theme.css'
 
 const MainMenu = ({ location }) => {
-  const [mobileActive, setMobileActive] = useState(false)
-  const pRef = useRef()
-  const handleMobileToggle = () => setMobileActive(!mobileActive)
-  const handleClickOutside = () => {
-    if (mobileActive) {
-      return handleMobileToggle()
-    }
-    return false
-  }
-  useOutsideClick(pRef, handleClickOutside)
-
   const isActive = page => {
     const { pathname } = location
     return page.test(pathname)
   }
 
   return (
-    <div className={theme.navContainer} ref={pRef}>
-      <button onClick={handleMobileToggle} className={theme.contactMobile} type="button">
-        {mobileActive ? <CloseIcon /> : <Hambuger />}
-      </button>
-      <ul className={clsx(theme.nav, mobileActive && theme.mobileActive)}>
+    <div className={theme.navContainer}>
+      <ul className={theme.nav}>
         <li className={theme.navItem}>
           <Link
             className={clsx(theme.navItemLink, isActive(/^\/$/) && theme.active)}
             to="/"
-            onClick={handleClickOutside}
           >
             Dashboard
           </Link>
@@ -44,7 +26,6 @@ const MainMenu = ({ location }) => {
           <Link
             className={clsx(theme.navItemLink, isActive(/search/) && theme.active)}
             to="/search"
-            onClick={handleClickOutside}
           >
             Job&nbsp;Search
           </Link>
@@ -53,18 +34,8 @@ const MainMenu = ({ location }) => {
           <Link
             className={clsx(theme.navItemLink, isActive(/settings/) && theme.active)}
             to="/settings"
-            onClick={handleClickOutside}
           >
             My&nbsp;Profile
-          </Link>
-        </li>
-        <li className={clsx(theme.navItem, theme.mobileOnly)}>
-          <Link
-            className={clsx(theme.navItemLink, isActive(/contact/) && theme.active)}
-            to="/contact"
-            onClick={handleClickOutside}
-          >
-            Contact
           </Link>
         </li>
       </ul>
