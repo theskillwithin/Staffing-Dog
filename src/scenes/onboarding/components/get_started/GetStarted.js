65e35edfd2e2c0b642421e5fffe8e508bf7ccf93
theskillwithin <austinppeterson@gmail.com>
2019-03-04 22:48:16 -0700

    convert to use hooks

--- src/scenes/onboarding/components/get_started/GetStarted.js
+++ src/scenes/onboarding/components/get_started/GetStarted.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import { func, string, object } from 'prop-types'
 import { withRouter } from 'react-router-dom'
 import clsx from 'clsx'
@@ -12,131 +12,126 @@ import StarTitle from '@sdog/components/star_title'
 
 import theme from './theme.css'
 
-class GetStarted extends Component {
-  componentDidMount() {
-    this.props.setStep(false)
-  }
+const GetStarted = ({ type, setStep }) => {
+  useEffect(() => {
+    setStep(false)
+  })
 
-  renderSVG = (SVGComponent, type = 'professional') => {
-    if (this.props.type === type) {
+  const renderSVG = (SVGComponent, isType = 'professional') => {
+    if (type === isType) {
       return SVGComponent
     }
 
     return null
   }
 
-  render() {
-    if (!this.props.type) {
-      return (
-        <div className={clsx(theme.gettingStarted, theme.gettingStartedSplit)}>
-          <div className={theme.left}>
-            <div>
-              <Person className={clsx(theme.group, theme.person, theme.man)} name="man" />
-              <Person
-                className={clsx(theme.group, theme.person, theme.woman3)}
-                name="woman3"
-              />
-              <Person
-                className={clsx(theme.group, theme.person, theme.woman2)}
-                name="woman2"
-              />
-            </div>
-            <Button round className={theme.letsGetStartedButton}>
-              Get Started as a Professional{' '}
-              <span>
-                <Arrow small color="white" />
-              </span>
-            </Button>
-          </div>
-          <div className={theme.divider} />
-          <div className={theme.right}>
-            <div>
-              <Svg name="dentist_chair" className={clsx(theme.group, theme.chair)} />
-
-              <Person
-                className={clsx(theme.group, theme.person, theme.woman)}
-                name="woman"
-              />
-            </div>
-            <Button round className={theme.letsGetStartedButton}>
-              Get Started as a Practice{' '}
-              <span>
-                <Arrow small color="white" />
-              </span>
-            </Button>
-          </div>
-        </div>
-      )
-    }
+  if (!type) {
     return (
-      <div className={theme.gettingStarted}>
-        <StarTitle title="Power to the Professional" />
-
-        <div className={theme.svgScene}>
-          <span className={clsx(theme.group, theme.groupPlant)}>
-            <div
-              className={clsx(theme.certification)}
-              dangerouslySetInnerHTML={{ __html: SVGCertification }}
-            />
-            <div
-              className={clsx(theme.certification)}
-              dangerouslySetInnerHTML={{ __html: SVGPlantTall }}
-            />
-          </span>
-
-          {this.renderSVG(
-            <Person className={clsx(theme.group, theme.person, theme.man)} name="man" />,
-          )}
-
-          {this.renderSVG(
-            <Person
-              className={clsx(theme.group, theme.person, theme.woman)}
-              name="woman"
-            />,
-          )}
-
-          {this.renderSVG(
-            <Person
-              className={clsx(theme.group, theme.person, theme.woman2)}
-              name="woman2"
-            />,
-          )}
-
-          {this.renderSVG(
+      <div className={clsx(theme.gettingStarted, theme.gettingStartedSplit)}>
+        <div className={theme.left}>
+          <div>
+            <Person className={clsx(theme.group, theme.person, theme.man)} name="man" />
             <Person
               className={clsx(theme.group, theme.person, theme.woman3)}
               name="woman3"
-            />,
-          )}
-
-          {this.renderSVG(
+            />
             <Person
-              className={clsx(theme.group, theme.person, theme.man2)}
-              name="man2"
-            />,
-          )}
+              className={clsx(theme.group, theme.person, theme.woman2)}
+              name="woman2"
+            />
+          </div>
+          <Button round className={theme.letsGetStartedButton}>
+            Get Started as a Professional{' '}
+            <span>
+              <Arrow small color="white" />
+            </span>
+          </Button>
+        </div>
+        <div className={theme.divider} />
+        <div className={theme.right}>
+          <div>
+            <Svg name="dentist_chair" className={clsx(theme.group, theme.chair)} />
 
-          {this.renderSVG(
-            <Person className={clsx(theme.group, theme.person, theme.man)} name="man" />,
-            'practice',
-          )}
-
-          {this.renderSVG(
-            <Svg name="dentist_chair" className={clsx(theme.group, theme.chair)} />,
-            'practice',
-          )}
-
-          {this.renderSVG(
             <Person
               className={clsx(theme.group, theme.person, theme.woman)}
               name="woman"
-            />,
-            'practice',
-          )}
+            />
+          </div>
+          <Button round className={theme.letsGetStartedButton}>
+            Get Started as a Practice{' '}
+            <span>
+              <Arrow small color="white" />
+            </span>
+          </Button>
         </div>
       </div>
     )
   }
+  return (
+    <div className={theme.gettingStarted}>
+      <StarTitle title="Power to the Professional" />
+
+      <div className={theme.svgScene}>
+        <span className={clsx(theme.group, theme.groupPlant)}>
+          <div
+            className={clsx(theme.certification)}
+            dangerouslySetInnerHTML={{ __html: SVGCertification }}
+          />
+          <div
+            className={clsx(theme.certification)}
+            dangerouslySetInnerHTML={{ __html: SVGPlantTall }}
+          />
+        </span>
+
+        {renderSVG(
+          <Person className={clsx(theme.group, theme.person, theme.man)} name="man" />,
+        )}
+
+        {renderSVG(
+          <Person
+            className={clsx(theme.group, theme.person, theme.woman)}
+            name="woman"
+          />,
+        )}
+
+        {renderSVG(
+          <Person
+            className={clsx(theme.group, theme.person, theme.woman2)}
+            name="woman2"
+          />,
+        )}
+
+        {renderSVG(
+          <Person
+            className={clsx(theme.group, theme.person, theme.woman3)}
+            name="woman3"
+          />,
+        )}
+
+        {renderSVG(
+          <Person className={clsx(theme.group, theme.person, theme.man2)} name="man2" />,
+        )}
+
+        {renderSVG(
+          <Person className={clsx(theme.group, theme.person, theme.man)} name="man" />,
+          'practice',
+        )}
+
+        {renderSVG(
+          <Svg name="dentist_chair" className={clsx(theme.group, theme.chair)} />,
+          'practice',
+        )}
+
+        {renderSVG(
+          <Person
+            className={clsx(theme.group, theme.person, theme.woman)}
+            name="woman"
+          />,
+          'practice',
+        )}
+      </div>
+    </div>
+  )
 }
 
 GetStarted.propTypes = {
