8ba5a38912f96a4de326b049a3a37bdd39fe5116
Will Ayers <will@staffing.dog>
2019-04-14 15:03:16 -0700

    Added definitions for job positions and positionTypes.

--- src/definitions/jobs.js
+++ src/definitions/jobs.js
@@ -15,5 +15,39 @@ export const positions = [
   { label: 'Other', value: 'other' },
 ]
 
+export const positionTypes = [
+  { label: 'Family & General Dentistry', value: 'family_general_dentist' },
+  { label: 'Anesthesiology', value: 'anesthesiology' },
+  { label: 'Endodontics', value: 'end0dontics' },
+  { label: 'Oral & Maxillofacial Surgery', value: 'oral_maxillofacial_surgery' },
+  { label: 'Orthodontics', value: 'orthodontics' },
+  { label: 'Pedodontics', value: 'pedodontics' },
+  { label: 'Periodontics', value: 'periodontics' },
+  { label: 'Prosthodontics', value: 'prosthodontics' },
+  { label: 'Radiology', value: 'radiology' },
+  { label: 'Other', value: 'other' },
+]
+
+export const positionTypesDentist = [
+  { label: 'Family & General Dentist', value: 'family_general_dentist' },
+  { label: 'Anesthesiologist', value: 'anesthesiologist' },
+  { label: 'Endodontist', value: 'endodontist' },
+  { label: 'Oral & Maxillofacial Surgeon', value: 'oral_maxillofacial_surgeon' },
+  { label: 'Orthodontist', value: 'orthodontist' },
+  { label: 'Pedodontist', value: 'pedodontist' },
+  { label: 'Periodontist', value: 'periodontist' },
+  { label: 'Prosthodontist', value: 'prosthodontist' },
+  { label: 'Radiologist', value: 'radiologist' },
+  { label: 'Other', value: 'other' },
+]
+
+export const positionTypesByPostion = {
+  default: [...positionTypes],
+  dentist: [...positionTypesDentist],
+}
+
+export const getPositionTypesByPosition = position =>
+  positionTypesByPostion[position] || positionTypesByPostion.default
+
 export const defineJob = (definekey, key) =>
   get(definitions, `${definekey}.${key}`, startCase(key))
