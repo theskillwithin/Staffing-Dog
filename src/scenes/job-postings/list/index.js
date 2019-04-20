1b59199e671ce98549696b2496a1b7dd28b060ad
Will Ayers <will@staffing.dog>
2019-04-19 17:35:04 -0700

    Filter job postings by status.

--- src/scenes/job-postings/list/index.js
+++ src/scenes/job-postings/list/index.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { func, bool, string, oneOfType, shape, array } from 'prop-types'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { withRouter, Link } from 'react-router-dom'
 import { useDocumentTitle } from '@sdog/utils/document'
+import qs from 'qs'
 import get from 'lodash/get'
+import filter from 'lodash/filter'
+import find from 'lodash/find'
 import clsx from 'clsx'
 import Card from '@sdog/components/card'
 import Filter from '@sdog/components/filter'
@@ -22,37 +25,59 @@ import appTheme from '../../app/theme.css'
 
 import theme from './theme.css'
 
-const JobPostings = ({ getUserJobs, jobs, loading }) => {
+const JobPostings = ({ getUserJobs, jobs, loading, location, history }) => {
   useDocumentTitle('Job Postings')
-  const [filter, setFilter] = useState({ label: 'Active', value: 'Active' })
+  const searchParams = qs.parse((location.search || '').substr(1))
+  const [filterValue, setFilter] = useState(searchParams.filter || 'active')
 
   useEffect(() => void getUserJobs(), [])
 
+  useEffect(
+    () => {
+      history.push(`${location.pathname}?${qs.stringify({ filter: filterValue })}`)
+    },
+    [filterValue],
+  )
+
   const filterOptions = [
-    { label: 'Active', value: 'Active' },
-    { label: 'Inactive', value: 'Inactive' },
+    { label: 'Active', value: 'active' },
+    { label: 'Scheduled', value: 'scheduled' },
+    { label: 'Draft', value: 'draft' },
+    { label: 'Deleted', value: 'deleted' },
   ]
+
+  const filterJobs = job => {
+    if ('active' === filterValue) {
+      return job.status !== 'deleted'
+    }
+
+    return job.status === filterValue
+  }
+
+  const filteredJobs = filter(jobs.posts || [], filterJobs)
 
   return (
     <div className={clsx(appTheme.pageContent, theme.container)}>
       <header className={theme.pageActions}>
         <Filter
-          onChange={value => setFilter(value)}
-          value={filter}
+          onChange={value => setFilter(value.value)}
+          value={find(filterOptions, ({ value }) => value === filterValue)}
           options={filterOptions}
         />
+
         <Link to="/job-postings/create">
           <Button>Post New Job</Button>
         </Link>
       </header>
+
       {loading ? (
         <div>
           <Spinner />
         </div>
       ) : (
         <div className={theme.jobs}>
-          {jobs && jobs.posts && jobs.posts.length ? (
-            jobs.posts.map(job => (
+          {filteredJobs.length ? (
+            filteredJobs.map(job => (
               <Card key={`job-posting-${job.id}`} type="large">
                 <Link
                   to={`/search/job/${job.slug}`}
@@ -108,7 +133,8 @@ const JobPostings = ({ getUserJobs, jobs, loading }) => {
             ))
           ) : (
             <h4>
-              No job postings. <Link to="/job-postings/create">Create one now!</Link>
+              No job postings{`${filterValue && ' matching your filters'}`}.{' '}
+              <Link to="/job-postings/create">Create one now!</Link>
             </h4>
           )}
         </div>
@@ -122,6 +148,8 @@ JobPostings.propTypes = {
   loading: bool.isRequired,
   error: oneOfType([bool, string]),
   getUserJobs: func.isRequired,
+  location: shape({ search: string.isRequired }).isRequired,
+  history: shape({ push: func.isRequired }).isRequired,
 }
 
 export const mapStateToProps = state => ({
@@ -132,7 +160,9 @@ export const mapStateToProps = state => ({
 
 export const mapActionsToProps = { getUserJobs: getUserJobsAction }
 
-export default connect(
-  mapStateToProps,
-  mapActionsToProps,
-)(JobPostings)
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapActionsToProps,
+  )(JobPostings),
+)
