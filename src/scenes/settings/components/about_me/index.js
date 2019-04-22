456eb9a7016b6404131b6fdb2ce87ab90960914e
theskillwithin <austinppeterson@gmail.com>
2019-04-21 22:00:51 -0600

    quick fixes

--- src/scenes/settings/components/about_me/index.js
+++ src/scenes/settings/components/about_me/index.js
@@ -116,6 +116,17 @@ const SettingsAboutMe = ({
 
   const isNotPractice = get(profile, 'user.type', 'professional') !== 'practice'
 
+  const professionDropdownInit = find(positions, {
+    value: get(profile, 'meta.summary.profession.type', ''),
+  })
+
+  const typeDropdownInit = professionDropdownInit
+    ? {
+        label: professionDropdownInit ? professionDropdownInit.label : {},
+        value: get(profile, 'meta.summary.profession.type', ''),
+      }
+    : ''
+
   const [form, setForm] = React.useState({
     profile: {
       description: profile.description,
@@ -148,12 +159,7 @@ const SettingsAboutMe = ({
         },
         summary: {
           profession: {
-            type: {
-              label: find(positions, {
-                value: get(profile, 'meta.summary.profession.type', ''),
-              }).label,
-              value: get(profile, 'meta.summary.profession.type', ''),
-            },
+            type: typeDropdownInit,
             specialty: get(profile, 'meta.summary.profession.specialty', ''),
           },
         },
