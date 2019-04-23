55d54962ae30eb28bf07e069ec8b5133c9711b2b
Will Ayers <will@staffing.dog>
2019-04-23 16:38:14 -0700

    Removed useMemo and switched out code for reference to function in utils/jobs

--- src/scenes/dashboard/components/job_schedule/event/List.js
+++ src/scenes/dashboard/components/job_schedule/event/List.js
@@ -1,27 +1,11 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import { string, oneOfType, object, array } from 'prop-types'
-import isArray from 'lodash/isArray'
+import { getJobsByUserType } from '@sdog/utils/jobs'
 
 import Event from './index'
 
-export function getJobsByUserType({ userType, jobs }) {
-  if (isArray(jobs)) {
-    return jobs
-  }
-
-  if ('professional' === userType) {
-    return jobs.scheduled
-  }
-
-  if ('practice' === userType) {
-    return jobs.posts || []
-  }
-
-  return []
-}
-
 export const EventList = ({ userType, jobs }) => {
-  const jobList = useMemo(() => getJobsByUserType({ userType, jobs }, [userType, jobs]))
+  const jobList = getJobsByUserType({ userType, jobs })
 
   return jobList.map((event, eventIndex) => (
     <Event userType={userType} key={event.id} event={event} open={eventIndex === 0} />
